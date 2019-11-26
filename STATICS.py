a7de4c0a12de2ad2abd4b4560b4d47db3c8d6b89
Frederik Gelder <frederik.gelder@freenet.de>
2019-11-27 00:47:07 +0100

    trying subdirs again, now that i know it was the modset all along

--- STATICS.py
+++ STATICS.py
@@ -4,4 +4,4 @@ ARMA3APPID = 107410
 
 STEAMFOLDER = '/home/moonlight/.steam'
 ARMA3SERVERDIR = os.path.join(STEAMFOLDER, 'SteamApps/common/Arma 3 Server')
-ARMA3MODSDIR = ARMA3SERVERDIR  # apparently subfolders dont work...
+ARMA3MODSDIR = os.path.join(ARMA3SERVERDIR, 'mods')
