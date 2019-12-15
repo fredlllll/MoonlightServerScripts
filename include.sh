e29ec1ec7949f02fb944d64f5cb4583975537318
Frederik Gelder <frederik.gelder@freenet.de>
2019-12-15 01:56:57 +0100

    adding user and group to replaceable stuff for service file

--- include.sh
+++ include.sh
@@ -2,5 +2,8 @@
 
 #this is to make the update scripts etc work without having to update them every time i change the service name
 
+serviceuser=moonlight
+servicegroup=moonlight
+
 servicename=moonlightdashboard
 servicedescription="Moonlight dashboard"
--- installservice.sh
+++ installservice.sh
@@ -6,6 +6,8 @@ cp unit.service /tmp/unit.service
 
 #replace things in unit file
 sed -i -e "s~__workingdir__~${PWD}~g" /tmp/unit.service
+sed -i -e "s~__user__~${serviceuser}~g" /tmp/unit.service
+sed -i -e "s~__group__~${servicegroup}~g" /tmp/unit.service
 sed -i -e "s~__servicedescription__~${servicedescription}~g" /tmp/unit.service
 
 sudo cp /tmp/unit.service /etc/systemd/system/${servicename}.service
