26cc6a4f08a2680eb49e2622419c022add9ed850
Frederik Gelder <frederik.gelder@freenet.de>
2019-12-15 01:53:55 +0100

    have to use ~ cause § isnt in ascii

--- installservice.sh
+++ installservice.sh
@@ -5,8 +5,8 @@ source include.sh
 cp unit.service /tmp/unit.service
 
 #replace things in unit file
-sed -i -e "s§__workingdir__§${PWD}§g" /tmp/unit.service
-sed -i -e "s§__servicedescription__§${servicedescription}§g" /tmp/unit.service
+sed -i -e "s~__workingdir__~${PWD}~g" /tmp/unit.service
+sed -i -e "s~__servicedescription__~${servicedescription}~g" /tmp/unit.service
 
 sudo cp /tmp/unit.service /etc/systemd/system/${servicename}.service
 rm /tmp/unit.service
