1eae96dcbf64fb9ec392088685e8842ae513819b
Frederik Gelder <frederik.gelder@freenet.de>
2022-02-13 03:46:29 +0100

    make run as sudo

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #authbind would be necessary if the application binds to port 80 without root rights on linux
 source .venv/bin/activate
-#TODO: i know having this process in sudo is risky, but i have some functionality that needs sudo rights and it dont have time to make a clean solution for now
-sudo python3.8 -m main
+python3.8 -m main
 deactivate
