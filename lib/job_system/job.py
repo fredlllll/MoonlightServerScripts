ef7003b4cfa7a2f92c642515035d9735bd2b07e7
Frederik Gelder <frederik.gelder@freenet.de>
2022-02-19 02:44:14 +0100

    fuck this

--- lib/job_system/job.py
+++ lib/job_system/job.py
@@ -17,9 +17,10 @@ class Job:
         self._save_info()
 
     def _save_info(self):
-        task = asyncio.ensure_future(self._job_info.save())
-        while not task.done():
-            time.sleep(0.0001)
+        #task = asyncio.ensure_future(self._job_info.save())
+        #while not task.done():
+        #    time.sleep(0.0001)
+
         # self._task = asyncio.create_task(self._job_info.save())
         # time.sleep(0.1)
         pass  # TODO: find a way to fucking save this
