865de7db576daf3772a28616ab9eb3e4b741d042
Frederik Gelder <frederik.gelder@freenet.de>
2022-02-19 01:42:41 +0100

    next try

--- lib/job_system/job.py
+++ lib/job_system/job.py
@@ -13,12 +13,17 @@ class Job:
         self._job_info.error = 'None'
         self._job_info.output = ''
         self._job_info.progress = 0
-        asyncio.get_event_loop().run_until_complete(self._job_info.save())
+
+        task = asyncio.get_event_loop().create_task(self._job_info.save())
+        while not task.done():
+            pass
 
     def execute(self):
         self._job_info.start_timestamp = time.time()
         self._job_info.status = EXECUTING
-        asyncio.get_event_loop().run_until_complete(self._job_info.save())
+        task = asyncio.get_event_loop().create_task(self._job_info.save())
+        while not task.done():
+            pass
         try:
             self._run()
             self._job_info.status = FINISHED
@@ -27,11 +32,15 @@ class Job:
             self._job_info.error = traceback.format_exc()
         self._job_info.end_timestamp = time.time()
         self._job_info.progress = 1
-        asyncio.get_event_loop().run_until_complete(self._job_info.save())
+        task = asyncio.get_event_loop().create_task(self._job_info.save())
+        while not task.done():
+            pass
 
     def _update_progress(self, progress=0):
         self._job_info.progress = progress
-        asyncio.get_event_loop().run_until_complete(self._job_info.save())
+        task = asyncio.get_event_loop().create_task(self._job_info.save())
+        while not task.done():
+            pass
 
     def _run(self):
         pass
