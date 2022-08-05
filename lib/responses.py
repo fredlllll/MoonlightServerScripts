f37d2d421289ffd53652c5bb2eae1e57eab01b8f
Frederik Gelder <frederik.gelder@freenet.de>
2022-08-05 22:27:48 +0200

    responses

---
+++ lib/responses.py
@@ -0,0 +1,7 @@
+from lib.jinja_templates import get_template
+from sanic.response import html
+
+
+def response_404(request, message=None):
+    template = get_template('404.html', request)
+    return html(template.render(message=message), status=404)
