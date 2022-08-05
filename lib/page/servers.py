cd9025e44ffd1f734e0612b4c5423bb9648a2f1d
Frederik Gelder <frederik.gelder@freenet.de>
2022-08-05 22:27:06 +0200

    make servers page also show modset

--- lib/page/servers.py
+++ lib/page/servers.py
@@ -2,6 +2,7 @@ import logging
 from sanic.response import html
 from lib.jinja_templates import get_template
 from lib.db.models.arma_3_server import Arma3Server
+from lib.db.models.arma_3_modset import Arma3Modset
 
 logger = logging.getLogger(__name__)
 
@@ -9,4 +10,17 @@ logger = logging.getLogger(__name__)
 async def servers(request):
     servers_ = Arma3Server.all()
 
-    return html(template.render(servers=servers_))    template = get_template("servers.html", request)
+    for s in servers_:
+        if s.modset_id:
+            try:
+                s.modset = Arma3Modset.find(s.modset_id).name
+            except:
+                s.modset = 'Not Found'
+        else:
+            s.modset = 'None'
+
+    template = get_template("servers.html", request)
+    return html(template.render(servers=servers_))
+
+async def servers_post(request):
+    pass
