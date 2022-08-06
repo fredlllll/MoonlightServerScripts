ecc77f7a1122d0979def7315fd176bb2c4a64b1f
Frederik Gelder <frederik.gelder@freenet.de>
2022-08-06 02:03:17 +0200

    deletion logic modset

--- lib/db/models/arma_3_modset.py
+++ lib/db/models/arma_3_modset.py
@@ -1,7 +1,8 @@
-from lib.db.models.model import Model
+from lib.db.models.model_deletion_logic import MongoModelDeletionLogic
+from lib.db.models.arma_3_server import Arma3Server
 
 
-class Arma3Modset(Model):
+class Arma3Modset(MongoModelDeletionLogic):
     @classmethod
     def _get_collection_name(cls):
         return 'arma_3_modsets'
@@ -10,3 +11,10 @@ class Arma3Modset(Model):
         return [
             'name',
         ]
+
+    @classmethod
+    def _delete_from_record(cls, record):
+        servers = Arma3Server.where({'modset_id': record['id']})
+        for server in servers:
+            server.modset_id = None
+            server.save()
--- lib/page/modset.py
+++ lib/page/modset.py
@@ -4,6 +4,7 @@ from lib.apis.steam import get_downloaded_mods, get_mod_name
 from lib.jinja_templates import get_template
 from lib.db.models.arma_3_modset_mod import Arma3ModsetMod
 from lib.db.models.arma_3_modset import Arma3Modset
+from lib.db.models.arma_3_server import Arma3Server
 from lib.responses import response_404
 
 logger = logging.getLogger(__name__)
@@ -22,25 +23,29 @@ async def modset(request, modset_id):
 
 
 async def modset_post(request, modset_id):
-    args = request.form
-
     modset_ = Arma3Modset.find(modset_id)
     if modset_ is None:
         return response_404(request)
 
-    all_mods = get_downloaded_mods()
-    active_mods = [m.mod_steam_id for m in Arma3ModsetMod.where({'modset_id': modset_.id})]
+    args = request.form
+    action = args.get('action', None)
+    if action == 'update':
+        all_mods = get_downloaded_mods()
+        active_mods = [m.mod_steam_id for m in Arma3ModsetMod.where({'modset_id': modset_.id})]
 
-    for mod_id in all_mods:
-        active = args.get(f'mod_{mod_id}', False)
-        if mod_id in active_mods and not active:
-            mms = Arma3ModsetMod.where({'modset_id': modset_.id, 'mod_steam_id': mod_id})
-            for mm in mms:
-                mm.delete()
-        if mod_id not in active_mods and active:
-            mm = Arma3ModsetMod()
-            mm.modset_id = modset_id
-            mm.mod_steam_id = mod_id
-            mm.save()
+        for mod_id in all_mods:
+            active = args.get(f'mod_{mod_id}', False)
+            if mod_id in active_mods and not active:
+                mms = Arma3ModsetMod.where({'modset_id': modset_.id, 'mod_steam_id': mod_id})
+                for mm in mms:
+                    mm.delete()
+            if mod_id not in active_mods and active:
+                mm = Arma3ModsetMod()
+                mm.modset_id = modset_id
+                mm.mod_steam_id = mod_id
+                mm.save()
+    elif action == 'delete':
+        modset_.delete()
+        return redirect('/modsets')
 
     return redirect(f'/modsets/{modset_id}')
