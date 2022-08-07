27821f0e4299f11fa002684b392b487457cd29d7
Frederik Gelder <frederik.gelder@freenet.de>
2022-08-07 17:21:19 +0200

    response 403

--- lib/responses.py
+++ lib/responses.py
@@ -2,6 +2,11 @@ from lib.jinja_templates import get_template
 from sanic.response import html
 
 
+def response_403(request, message: str = None):
+    template = get_template('403.html', request)
+    return html(template.render(message=message), status=403)
+
+
 def response_404(request, message: str = None):
     template = get_template('404.html', request)
     return html(template.render(message=message), status=404)
