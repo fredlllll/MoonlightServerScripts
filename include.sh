9fc31b4bb5e04d9ab7917500f62887439675d56b
Frederik Gelder <frederik.gelder@freenet.de>
2022-08-09 01:22:51 +0200

    have to use root as user for service, otherwise i cant write to service files

--- include.sh
+++ include.sh
@@ -2,8 +2,8 @@
 
 #this is to make the update scripts etc work without having to update them every time i change the service name
 
-serviceuser=moonlight
-servicegroup=moonlight
+serviceuser=root
+servicegroup=root
 
 servicename=moonlightdashboard
 servicedescription="Moonlight dashboard"
