b155e102107273d5d4b87d402877ea38ee845898
Frederik Gelder <frederik.gelder@freenet.de>
2022-08-16 03:08:51 +0200

    use python 3.10

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #authbind would be necessary if the application binds to port 80 without root rights on linux
 source .venv/bin/activate
-python3.8 -m main
+python3.10 -m main
 deactivate
