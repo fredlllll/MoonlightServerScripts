c5d68fee9418e4bb0fd3fbf8fe738dad8f6b4d47
Frederik Gelder <frederik.gelder@freenet.de>
2022-08-17 02:28:25 +0200

    fix again

--- lib/page/modset.py
+++ lib/page/modset.py
@@ -19,7 +19,7 @@ async def modset(request, modset_id):
     all_mods = {i: get_mod_name(i) for i in get_downloaded_mods()}
 
     modset_.active_mods = [m.mod_steam_id for m in Arma3ModsetMod.where({'modset_id': modset_.id})]
-    modset_.active_uninstalled_mods = {k: v for k, v in modset_.active_mods if k not in all_mods}
+    modset_.active_uninstalled_mods = {k: get_mod_name(k) for k in modset_.active_mods if k not in all_mods}
 
     return html(template.render(modset=modset_, all_mods=all_mods))
 
