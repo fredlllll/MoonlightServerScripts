f0aa19033cc25f435d8dbfcb3f5be171ce74e6cb
Frederik Gelder <frederik.gelder@freenet.de>
2022-11-08 21:47:00 +0100

    add rundatabase script

---
+++ rundatabase.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+if [ ! -f mongorootpasswd ]
+then
+  echo "missing password file mongorootpasswd"
+  exit;
+fi
+
+docker volume create local-mongo-data
+docker run -d --name local-mongo -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD_FILE=mongorootpasswd -v local-mongo-data:/data/db
