778e0593d3401785adb9406dbb83a47d93e88e0b
Frederik Gelder <frederik.gelder@freenet.de>
2023-05-06 23:28:55 +0200

    fix log tailer

--- lib/process_log_keeper.py
+++ lib/process_log_keeper.py
@@ -4,7 +4,7 @@ import asyncio
 
 
 class ProcessLogKeeper:
-    def __init__(self, commandline, websocket_channel):
+    def __init__(self, commandline: list, websocket_channel):
         self.commandline = commandline
         self.websocket_channel = websocket_channel
         self.log = ""
@@ -22,8 +22,8 @@ class ProcessLogKeeper:
 
     async def start(self):
         if Settings.debug_windows:
-            self.process_tailer = ProcessTailer(None,self.websocket_channel)
+            self.process_tailer = ProcessTailer(None, self.websocket_channel)
         else:
-            self.process_tailer = ProcessTailer(await asyncio.create_subprocess_exec(self.commandline, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE), self.websocket_channel)
+            self.process_tailer = ProcessTailer(await asyncio.create_subprocess_exec(*self.commandline, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE), self.websocket_channel)
         self.process_tailer.text_handler = self.get_text_handler()
         self.process_tailer.start()
