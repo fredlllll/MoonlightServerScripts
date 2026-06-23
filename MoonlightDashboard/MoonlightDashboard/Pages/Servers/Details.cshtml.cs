b69ea5b047b5b6cd621a05176d5611aeba398631
Frederik Gelder <frederik.gelder@freenet.de>
2026-06-23 15:58:13 +0200

    started server pages

--- MoonlightDashboard/MoonlightDashboard/Pages/Servers/Details.cshtml.cs
+++ MoonlightDashboard/MoonlightDashboard/Pages/Servers/Details.cshtml.cs
@@ -1,12 +1,51 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoonlightDashboard.Database;
+using MoonlightDashboard.Database.Models;
+
 
 namespace MoonlightDashboard.Pages.Servers
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        public Arma3Server Server = null!;
+        public List<Arma3Modset> Modsets = null!;
+        private DatabaseContext db;
+
+        public DetailsModel(DatabaseContext db)
         {
+            this.db = db;
+        }
+
+        public void OnGet(string id)
+        {
+            Server = db.Arma3Servers.First(s => s.Id == id);
+            Modsets = db.Arma3Modsets.ToList();
+        }
+
+        public void OnPostSetModset(string id, string? modsetId)
+        {
+            if (string.IsNullOrWhiteSpace(modsetId))
+            {
+                modsetId = null;
+            }
+            Server = db.Arma3Servers.First(s => s.Id == id);
+            Server.ActiveModsetId = modsetId;
+            db.SaveChanges();
+            OnGet(id);
+        }
+
+        public void OnPostSetPort(string id, int port)
+        {
+            Server = db.Arma3Servers.First(s => s.Id == id);
+            Server.Port = port;
+            db.SaveChanges();
+            OnGet(id);
+        }
+
+        public void OnPostStart(string id)
+        {
+
         }
     }
 }
--- MoonlightDashboard/MoonlightDashboard/Pages/Servers/Index.cshtml.cs
+++ MoonlightDashboard/MoonlightDashboard/Pages/Servers/Index.cshtml.cs
@@ -1,12 +1,68 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoonlightDashboard.Database;
+using MoonlightDashboard.Database.Models;
+using MoonlightDashboard.Lib;
+using MoonlightDashboard.Services;
 
 namespace MoonlightDashboard.Pages.Servers
 {
     public class IndexModel : PageModel
     {
+        public List<Arma3Server> Servers = null!;
+        public Dictionary<string, string> ActiveModsetNames = null!;
+        private DatabaseContext db;
+
+        public IndexModel(DatabaseContext db)
+        {
+            this.db = db;
+        }
+
         public void OnGet()
         {
+            Servers = db.Arma3Servers.ToList();
+            ActiveModsetNames = new();
+
+            foreach (var server in Servers)
+            {
+                if (server.ActiveModsetId != null)
+                {
+                    var modset = db.Arma3Modsets.FirstOrDefault(m => m.Id == server.ActiveModsetId);
+                    if (modset != null)
+                    {
+                        ActiveModsetNames[server.Id] = modset.Name;
+                    }
+                    else
+                    {
+                        ActiveModsetNames[server.Id] = "Not Found";
+                    }
+                }
+            }
+        }
+
+        public void OnPostCreate(string name, int port)
+        {
+            var server = new Arma3Server()
+            {
+                Id = Util.GetNewId<Arma3Server>(),
+                Name = name,
+                Port = port
+            };
+            db.Arma3Servers.Add(server);
+            db.SaveChanges();
+            Redirect($"/Servers/{server.Id}");
+        }
+
+        public void OnPostUpdateServer()
+        {
+            JobService.EnqueueUpdateServer(db, null);
+            Redirect("/Jobs");
+        }
+
+        public void OnPostUpdateServerCreatorDlc()
+        {
+            JobService.EnqueueUpdateServer(db, "creatordlc");
+            Redirect("/Jobs");
         }
     }
 }
